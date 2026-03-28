1f8e2e48cbfaebdaf3d11034e145a41c01e90001
Taras Mazepa <git@okl.pw>
2026-03-28 14:13:05 -0700

    Optimize tbx-combine-files.sh

--- tbx-combine-files.sh
+++ tbx-combine-files.sh
@@ -21,20 +21,12 @@ find "$TARGET_DIR" -type f \
     | sort \
     | while read -r file; do
 
-    # Write the header
-    echo "file: $file" >> "$OUTPUT_FILE"
-    echo "" >> "$OUTPUT_FILE"
-
-    # Write the file content
-    cat "$file" >> "$OUTPUT_FILE"
-
-    # If the file is not empty and doesn't end with a newline, add one
-    if [ -s "$file" ] && [ "$(tail -c 1 "$file" | wc -l)" -eq 0 ]; then
-        echo "" >> "$OUTPUT_FILE"
-    fi
-
-    # Add an empty line after the file content
-    echo "" >> "$OUTPUT_FILE"
+    {
+      echo "file: $file"
+      echo ""
+      cat "$file"
+      echo ""
+    } >> "$OUTPUT_FILE"
 done
 
 pbcopy < "$OUTPUT_FILE"
